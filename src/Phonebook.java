d825b3cc5d83508e98b7cfb0237b70086cc47c27
Roman_M <80831520+motorin-romchik@users.noreply.github.com>
2022-11-09 21:26:00 +0700

    Initial commit

---
+++ src/Phonebook.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+public class Phonebook {
+    public static void main(String[] args) throws IOException {
+        Properties ht = new Properties();
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        String name,number;
+        FileInputStream fin = null;
+        boolean changed = false;
+
+        try {
+            fin = new FileInputStream("phonebook.dat");
+        } catch (FileNotFoundException e){
+            //
+        }
+
+        try {
+            if(fin!=null){
+                ht.load(fin);
+                fin.close();
+            }
+        }catch (IOException e){
+            System.out.println("Error of reading file!");
+        }
+
+        do {
+            System.out.println("Введите имя"+
+                    " ('выход' для завершения): ");
+            name = br.readLine();
+            if (name.equals("выход")) continue;
+            System.out.println("Введите номер: ");
+            number = br.readLine();
+            ht.put(name,number);
+            changed = true;
+        } while(!name.equals("выход"));
+        //search
+
+        do{
+            System.out.println("Введите имя для поиска"+
+                    " ('выход' для завершения):");
+            name = br.readLine();
+            if(name.equals("выход")) continue;
+            number = (String) ht.get(name);
+            System.out.println(number);
+
+        }while(!name.equals("выход"));
+    }
+}
